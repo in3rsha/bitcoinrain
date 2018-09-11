552ddd1c9b3ef8c5499f271173e708b895e2a549
Greg Walker <greg@learnmeabitcoin.com>
2018-09-11 18:20:11 +0100

    Added Bitcoin.connect convenience function that connects and takes care of the handshake.

--- lib/bitcoin.rb
+++ lib/bitcoin.rb
@@ -17,16 +17,60 @@ module Utils
     hex = ascii.each_byte.map {|c| c.to_s(16) }.join
     return hex
   end
+
+  # Add padding to create a fixed-size field (e.g. 4 => 00000004)
+  def self.field(field, size=4)
+    return field.to_s.rjust(size*2, '0')
+  end
 end
 
 module Bitcoin
+
+  # Quickly connect to the bitcoin network (take care of handshake as well)
+  def self.connect(ip, port=8333)
+    socket = Bitcoin::Protocol::Connection.new(ip, port)
+    puts "Welcome to the Bitcoin Network: #{socket.inspect}"
+
+    # Take care of the handshake
+    self.handshake(socket)
+
+    # Hand the socket over
+    return socket
+  end
+
+  def self.handshake(socket)
+    # send version
+    version = Bitcoin::Protocol::Message.version # create a version message to send
+    socket.write version.binary # Send binary across the wire
+    puts "version->"
+
+    # get the version
+    message = socket.gets_message
+    puts "<-#{message.type}"
+
+    # get the verack
+    message = socket.gets_message
+    puts "<-#{message.type}"
+
+    # reply to verack
+    verack = Bitcoin::Protocol::Message.new('verack') # "F9BEB4D9 76657261636b000000000000 00000000 5df6e0e2"
+    socket.write verack.binary
+    puts "verack->"
+
+    # Now hand the socket back
+    return socket
+  end
 
   module Protocol
 
     MAGIC_BYTES = "f9beb4d9"
 
     class Connection < TCPSocket
+      attr_reader :ip, :port
+
       def initialize(ip, port=8333)
+        @ip = ip
+        @port = port
         super ip, port
       end
 
@@ -87,6 +131,28 @@ module Bitcoin
 
         return Message.new(type, payload)
       end
+
+      def self.version(version=70015, services=13, lastblock=0)
+        # https://github.com/bitcoin/bitcoin/blob/master/src/version.h
+        # 60002 = 62EA0000
+        # 70011 = 7b110100
+        # 70012 = 7c110100
+        # 70013 = 7d110100 <- ! socket.recv(0) on a verack payload blocks on this version
+        # 70014 = 7e110100 <-   this is what PHP script uses
+        # 70015 = 7f110100 <-   this is the version of the remote node
+
+        payload =  Utils.reversebytes(Utils.field(version.to_s(16))) #  protocol version
+        payload += "0D 00 00 00 00 00 00 00" # services (NODE_NETWORK = 1) (Segwit: NODE_WITNESS = (puts 1 << 3))
+        payload += Time.now.to_i.to_s(16).rjust(16, '0').upcase.scan(/../).reverse.join(" ")       # unixtime
+        payload += "01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF 2E 13 89 4A 20 8D" # remote address
+        payload += "01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF 7F 00 00 01 20 8D" # local address
+        payload += Utils.reversebytes(rand(18446744073709551615).to_s(16)) # nonce
+        payload += "00" # sub-version string
+        payload += Utils.reversebytes(Utils.field(lastblock.to_s(16), 4)) # last block we have
+
+        return Message.new('version', payload.delete(" "))
+      end
+
     end
 
   end
