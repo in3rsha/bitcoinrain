2bd7d99151bfa323b3acda1e1d226f2be42023b5
Greg Walker <greg@learnmeabitcoin.com>
2018-11-18 14:23:24 +0000

    Updated tmuxinator config, and added install.sh script that links it to the common tmuxinator config folder so that the project can be started from anywhere with `tmuxinator start bitcoinrain`

---
+++ install.sh
@@ -0,0 +1,2 @@
+# link tmuxinator start config to main tmuxinator folder (can start this project from anywhere then)
+ln -svf ~/inersha/projects/bitcoinrain/.tmuxinator.yml ~/.config/tmuxinator/bitcoinrain.yml
