8b2f1985ccb961ea19d7c93029cb83f88e67e6ca
Greg Walker <greg@learnmeabitcoin.com>
2018-11-19 17:08:48 +0000

    install.sh now creates main tmuxinator config folder if it doesn't exist (so that the config can be copied there).

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 # link tmuxinator start config to main tmuxinator folder (can start this project from anywhere then)
+mkdir -p ~/.config/tmuxinator
 ln -svf ~/inersha/projects/bitcoinrain/.tmuxinator.yml ~/.config/tmuxinator/bitcoinrain.yml
