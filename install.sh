86b07df98d8077291cae2cd8d8df5c397a81433e
Greg Walker <greg@learnmeabitcoin.com>
2019-04-12 11:19:26 +0100

    untracked unnecessary files

--- install.sh
+++
@@ -1,3 +0,0 @@
-# link tmuxinator start config to main tmuxinator folder (can start this project from anywhere then)
-mkdir -p ~/.config/tmuxinator
-ln -svf ~/inersha/projects/bitcoinrain/.tmuxinator.yml ~/.config/tmuxinator/bitcoinrain.yml
